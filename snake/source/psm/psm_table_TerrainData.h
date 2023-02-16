d62a2974b3122105dee6b37ca16b1e9791f95898
R2Road <studior2road@gmail.com>
2023-02-17 06:16:17 +0900

    using

--- snake/source/psm/psm_table_TerrainData.h
+++ snake/source/psm/psm_table_TerrainData.h
@@ -11,27 +11,29 @@ namespace psm_table
 	struct TerrainData
 	{
 	public:
+		using CellT = psm::eCellType;
+
 		TerrainData() : width( 1 ), height( 1 ), data( { 0 } )
 		{}
 
-		TerrainData( const int new_width, const int new_height, const std::initializer_list<psm::eCellType> new_data ) :
+		TerrainData( const int new_width, const int new_height, const std::initializer_list<CellT> new_data ) :
 			width( new_width )
 			, height( new_height )
-			, data( new_width * new_height, psm::eCellType::Open )
+			, data( new_width * new_height, CellT::Open )
 		{
 			R2ASSERT( 0 < width , "" );
 			R2ASSERT( 0 < height, "" );
 			
 			memcpy_s(
 				&data[0]
-				, data.size() * sizeof( psm::eCellType )
+				, data.size() * sizeof( CellT )
 				, &( *new_data.begin() )
-				, ( new_data.size() > data.size() ? data.size() * sizeof( psm::eCellType ) : new_data.size() * sizeof( psm::eCellType ) )
+				, ( new_data.size() > data.size() ? data.size() * sizeof( CellT ) : new_data.size() * sizeof( CellT ) )
 			);
 		}
 
 		int width;
 		int height;
-		std::vector<psm::eCellType> data;
+		std::vector<CellT> data;
 	};
 }
